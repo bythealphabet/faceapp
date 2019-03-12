37860b4eef286a87903454dadb0fb1d3533b26b7
bythealphabet <bythealphabet@gmail.com>
2019-03-11 23:51:26 -0400

    db_url

--- server.js
+++ server.js
@@ -10,12 +10,21 @@ const image = require('./controllers/image.js')
 
 const db_url = 'postgres://ccmbzxmwebvnvi:d2bfc18a9fa4681912f49ee928e6f8d8f9c7304b4a7be5e355810aff5cb36a77@ec2-75-101-133-29.compute-1.amazonaws.com:5432/dbmcofg07rqgur'
 
-
 const db = knex({
   client: 'pg',
-  connection: db_url,
-  ssl: true
+  connection: {
+    connectionString: db_url,
+  },
+  production: {
+    client: 'postgresql',
+    connectionString: {
+      database: db_url,
+    },
+    ssl: true
 });
+
+
+
 
 const app = express()
 app.use(bodyParser.json())
