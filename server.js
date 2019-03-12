4bd2ac8bbc2b713e6f6d9d100fef371555dd95f4
bythealphabet <bythealphabet@gmail.com>
2019-03-12 17:02:32 -0400

    npm install all the pagackegs again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt-nodejs')
+const bcrypt = require('bcrypt')
 const cors = require('cors')
 const knex = require('knex')
 const register = require('./controllers/register.js')
@@ -8,26 +8,22 @@ const signin = require('./controllers/signin.js')
 const profile = require('./controllers/profile.js')
 const image = require('./controllers/image.js')
 
-// const dburl = 'postgres://ccmbzxmwebvnvi:d2bfc18a9fa4681912f49ee928e6f8d8f9c7304b4a7be5e355810aff5cb36a77@ec2-75-101-133-29.compute-1.amazonaws.com:5432/dbmcofg07rqgur'
-
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL
-  },
-  ssl:true
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'xb1RL2-',
+    database : 'smartbrain'
+  }
 });
-
-
-
-
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
 app.get ('/', (req,res)=>{
-	res.send('it is working!')
+	res.send(database.users)
 })
 
 app.post('/signin',(req, res)=>{signin.handleSignin(req, res, db, bcrypt)})
@@ -36,8 +32,8 @@ app.get('/profile/:id', (req, res)=>{profile.handleProfile(req, res, db)})
 app.put('/image', (req, res)=>{image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)})
 
-app.listen (3000, ()=>{
+app.listen(3000, ()=>{
 	console.log(`app is running on port 3000`)
 })
 
-
+console.log()
