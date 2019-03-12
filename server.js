3d72755e253cf52ca6358c90755393649087f4e1
bythealphabet <bythealphabet@gmail.com>
2019-03-12 17:20:12 -0400

    added the path:

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get ('/', (req,res)=>{
-	res.send(database.users)
+	res.send("Im here and running!")
 })
 
 app.post('/signin',(req, res)=>{signin.handleSignin(req, res, db, bcrypt)})
