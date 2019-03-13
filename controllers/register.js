2131b0afe93505aa58568888d99c60f45a8557ef
bythealphabet <bythealphabet@gmail.com>
2019-03-13 16:07:03 -0400

    made mayor changes to the register.js adding querystrings and taking out knex

--- controllers/register.js
+++ controllers/register.js
@@ -1,37 +1,37 @@
+ const handleRegister = (req, res, pool, bcrypt)=>{
+		(async () => {
+		const {email, name, password} = req.body
+		if(!email || !name || !password){
+			return res.status(400).json('incorrect form submission')
+		}
+		const hash = bcrypt.hashSync(password)
+	  	const client = await pool.connect()
+		  try {
+		    await client.query('BEGIN')
 
+		    const insertLogin = {
+		      text: 'INSERT INTO login(email, hash) VALUES($1, $2) RETURNING id',
+		      values: [email, hash],
+		    }
+		    const {rows} = await client.query(insertLogin)
 
-const handleRegister = (req, res, db, bcrypt)=>{
-	const {email, name, password} = req.body
+		    const insertUser = {
+		      text: 'INSERT INTO users(id, name, email, joined) VALUES ($1, $2, $3, $4) RETURNING *',
+		      values: [rows[0].id, name, email, new Date()],
+		    }
+		    const userData = await client.query(insertUser)
+			await res.json(userData.rows[0])
 
-	if(!email || !name || !password){
-		return res.status(400).json('incorrect form submission')
+			console.log(userData.rows[0])
+		    await client.query('COMMIT')
+		  } catch (e) {
+		    await client.query('ROLLBACK')
+		    throw e
+		  } finally {
+		    client.release()
+		  }
+		})().catch(e => console.error(e.stack))
 	}
-	const hash = bcrypt.hashSync(password);
-		db.transaction(trx =>{
-			trx.insert({
-				hash: hash,
-				email: email
-			})
-			.into('login')
-			.returning('email')
-			.then(loginEmail =>{
-				return trx('users')
-				.returning('*')
-				.insert({	
-						email: loginEmail[0],
-						name: name,
-						joined: new Date()
-					})
-				.then(user =>{
-					res.json(user[0])
-				})
-			})
-			.then(trx.commit)
-			.catch(trx.rollback)
-		})
-		
-			.catch(err => res.status(400).json('unable to register'))
-}
 
 module.exports = {
 	handleRegister: handleRegister
--- server.js
+++ server.js
@@ -1,63 +1,64 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
-// const cors = require('cors')
-const knex = require('knex')
+const { Client, Pool } = require('pg')
 const register = require('./controllers/register.js')
-const signin = require('./controllers/signin.js')
-const profile = require('./controllers/profile.js')
-const image = require('./controllers/image.js')
+// const signin = require('./controllers/signin.js')
+// const profile = require('./controllers/profile.js')
+// const image = require('./controllers/image.js')
 
-const myDB = 'postgres://ccmbzxmwebvnvi:d2bfc18a9fa4681912f49ee928e6f8d8f9c7304b4a7be5e355810aff5cb36a77@ec2-75-101-133-29.compute-1.amazonaws.com:5432/dbmcofg07rqgur'
-
-
-const db = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'xb1RL2-',
-    database : 'smartbrain'
-  }
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
-// const db = knex({
-// 	client: 'pg',
-//   	production: {
-//     connection: myDB
-// 	},
-// 	ssl: true
-// });
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: '127.0.0.1',
+//   database: 'smartbrain',
+//   password: 'xb1RL2-',
+// })
+
+const db = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+// const db = new Client({
+//   user: 'postgres',
+//   host: '127.0.0.1',
+//   database: 'smartbrain',
+//   password: 'xb1RL2-',
+// })
+
+db.connect()
 
 const app = express()
 app.use(bodyParser.json())
-// app.use(cors())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
-app.get('/', function(req, res, next) {
-  // Handle the get for this route
-});
 
-app.post('/', function(req, res, next) {
- // Handle the post for this route
-});
+app.get ('/', (req,res)=>{'Im in'})
+// app.post('/signin',(req, res)=>{signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req, res)=>{register.handleRegister(req, res, pool, bcrypt)})
+// app.get('/profile/:id', (req, res)=>{profile.handleProfile(req, res, db)})
+// app.put('/image', (req, res)=>{image.handleImage(req, res, db)})
+// app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)})
 
-app.get ('/', (req,res)=>{res.send("Im here and running!")})
+// const text = 'INSERT INTO users(name, email, joined) VALUES($1, $2, $3) RETURNING *'
+// const values = ['sal', 'sal@gmail.com',new Date()]
 
-app.post('/signin',(req, res)=>{signin.handleSignin(req, res, db, bcrypt)})
-app.post('/register', (req, res)=>{register.handleRegister(req, res, db, bcrypt)})
-app.get('/profile/:id', (req, res)=>{profile.handleProfile(req, res, db)})
-app.put('/image', (req, res)=>{image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)})
+// client.query(text, values)
+//   .then(res => {
+//     console.log(res.rows[0])
+//   })
+//   .catch(e => console.error(e.stack))
 
-const PORT = process.env.PORT || 3000
-// const PORT = 3000
+///////////////////////////////////////////////////////////////////
 
-app.listen(PORT, ()=>{
-	console.log(`app is running on port ${ PORT }`)
+
+  	// app.post('/register', )
+
+app.listen(3000, ()=>{
+	console.log(`app is running on port 3000`)
 })
