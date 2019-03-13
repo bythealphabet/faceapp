71d653d1f78dedf66e2afb8438667aafc2c7eac6
bythealphabet <bythealphabet@gmail.com>
2019-03-12 21:25:37 -0400

    adding cors

--- server.js
+++ server.js
@@ -33,11 +33,18 @@ const app = express()
 app.use(bodyParser.json())
 // app.use(cors())
 
-app.options("/*", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.sendStatus(200);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
 });
 
 app.get ('/', (req,res)=>{res.send("Im here and running!")})
